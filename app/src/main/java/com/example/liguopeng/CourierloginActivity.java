1f269889c67ed4d3ca3a52ceaabd82df10a24bc0
gpl2016 <guopengli2016@163.com>
2019-01-15 12:49:40 +0800

    在首页添加了登录表，能够扫码了

--- app/src/main/java/com/example/liguopeng/CourierloginActivity.java
+++ app/src/main/java/com/example/liguopeng/CourierloginActivity.java
@@ -1,13 +1,37 @@
 package com.example.liguopeng;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.liguopeng.zxing.activity.CaptureActivity;
 
 public class CourierloginActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courierlogin);
+        btn=(Button)findViewById(R.id.btn_courier_scan);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission(CourierloginActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CourierloginActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                } else {
+                    Intent intent = new Intent(CourierloginActivity.this, CaptureActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 }
